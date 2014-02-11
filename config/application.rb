2160388b156d7c9d964344b02ffec85206383d19
France-Nor Brute <fbrute@gmail.com>
2014-02-11 19:49:41 -0400

    precompiled asset to false

--- config/application.rb
+++ config/application.rb
@@ -60,7 +60,8 @@ module Webphy
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false 
+    config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
